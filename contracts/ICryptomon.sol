d77fb693fa6e37e12ff8bccc34a9c562cd3e43c9
bp99 <bertalan.peter@bp99.eu>
2025-04-07 21:24:04 +0200

    refactor: improve solidity formatting

--- contracts/ICryptomon.sol
+++ contracts/ICryptomon.sol
@@ -1,10 +1,8 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-
 
 /**
  * @title Cryptomon Interface
@@ -43,16 +41,16 @@ interface ICryptomon is IERC721 {
      * @param name The name of the new monster.
      * @return monsterId The ID of the newly created monster.
      */
-    function createMonster(string calldata name) external returns (uint256);
+    function createMonster(string calldata name)
+        external returns (uint256);
 
     /**
      * @notice Get information about a monster.
      * @param monsterId The ID of the monster.
      * @return Monster The monster's details.
      */
-    function getMonster(uint256 monsterId) external view returns (
-        Monster memory
-    );
+    function getMonster(uint256 monsterId)
+        external view returns (Monster memory);
 
      // ============================
     //        BATTLE LOGIC
@@ -64,7 +62,10 @@ interface ICryptomon is IERC721 {
      * @param opponentMonsterId The ID of the opponent's monster.
      * @return battleId The ID of the created battle.
      */
-    function challengeMonster(uint256 myMonsterId, uint256 opponentMonsterId) external returns (uint256);
+    function challengeMonster(
+        uint256 myMonsterId,
+        uint256 opponentMonsterId
+    ) external returns (uint256);
 
     /**
      * @notice Accept a battle challenge.
@@ -90,9 +91,10 @@ interface ICryptomon is IERC721 {
      * @param monsterId The monster ID.
      * @return movesList An array of move names.
      */
-    function moves(uint256 monsterId) external view returns (string[] memory);
+    function moves(uint256 monsterId)
+        external view returns (string[] memory);
 
-   // ============================
+    // ============================
     //     TOURNAMENT LOGIC
     // ============================
 
@@ -102,25 +104,35 @@ interface ICryptomon is IERC721 {
      * @param levelLimit Optional level restriction.
      * @return tournamentId The ID of the newly created tournament.
      */
-    function createTournament(uint256 maxParticipants, uint256 levelLimit) external returns (uint256);
+    function createTournament(
+        uint256 maxParticipants,
+        uint256 levelLimit
+    ) external returns (uint256);
 
     /**
      * @notice Join a tournament with a monster.
      * @param tournamentId The tournament ID.
      * @param monsterId The monster to register.
      */
-    function joinTournament(uint256 tournamentId, uint256 monsterId) external;
+    function joinTournament(
+        uint256 tournamentId,
+        uint256 monsterId
+    ) external;
 
     /**
      * @notice Execute the next round of a tournament.
      * @param tournamentId The ID of the tournament.
      */
-    function executeTournamentRound(uint256 tournamentId) external;
+    function executeTournamentRound(uint256 tournamentId)
+        external;
 
     /**
      * @notice Claim the prize after winning the tournament.
      * @param tournamentId The ID of the tournament.
      * @param monsterId The winner monster ID.
      */
-    function claimTournamentPrize(uint256 tournamentId, uint256 monsterId) external;
+    function claimTournamentPrize(
+        uint256 tournamentId,
+        uint256 monsterId
+    ) external;
 }
